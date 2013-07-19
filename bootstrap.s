a360f748b27a7cf89dd268cdae223f8d6801c19a
Jonathan Lamothe <jonathan@jlamothe.net>
2013-07-18 20:43:17 -0400

    run main in user mode with 256 words of stack space

--- bootstrap.s
+++ bootstrap.s
@@ -1,4 +1,12 @@
         .global _start
 _start:
-        ldr sp, =0x07ffffff
-        bl main
+        ldr sp, =0x07fffffc
+        msr CPSR_c, #0xdf
+        ldr sp, =0x07fffffc
+        msr CPSR_c, #0xd3
+        ldr r0, =0x400
+        sub sp, sp, r0
+        mov r0, #0x10
+        msr SPSR, r0
+        ldr lr, =main
+        movs pc, lr        
