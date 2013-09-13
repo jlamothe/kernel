fddee51b024c533a674fa96f158b87b73031e3f2
Jonathan Lamothe <jonathan@jlamothe.net>
2013-09-12 23:04:49 -0400

    main function uses fork

--- main.c
+++ main.c
@@ -10,7 +10,15 @@
 
 int main(void)
 {
+    int fork_result;
     bwputs("Hello world!\n");
+    fork_result = fork();
+    if(fork_result < 0)
+        bwputs("Fork failed!\n");
+    else if(fork_result)
+        bwputs("Main thread!\n");
+    else
+        bwputs("Child thread!\n");
     return 0;
 }
 
